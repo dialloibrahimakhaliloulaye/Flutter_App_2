92c6ae3ba282c49485ec243e2f441ad229f341ad
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-11-02 22:38:24 +0000

    TextEding setting

--- lib/pages/users/users.page.dart
+++ lib/pages/users/users.page.dart
@@ -1,14 +1,68 @@
 import 'package:flutter/material.dart';
 
-class UsersPage extends StatelessWidget {
-  const UsersPage({Key? key}) : super(key: key);
+class UsersPage extends StatefulWidget {
+
+  @override
+  State<UsersPage> createState() => _UsersPageState();
+}
+
+class _UsersPageState extends State<UsersPage> {
+
+  TextEditingController queryTEC=TextEditingController();
+  String query='';
+  bool notVisible=false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("User", style: Theme.of(context).textTheme.headline3,),),
+      appBar: AppBar(title: Text("Users => ${query}", style: Theme.of(context).textTheme.headline6,),),
       body: Center(
-        child: Text("Users"),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                      child: TextFormField(
+                        obscureText: notVisible,
+                        onChanged: (value){
+                          setState(() {
+                            this.query=value;
+                          });
+                        },
+                        controller: queryTEC,
+                        decoration: InputDecoration(
+                          //icon: Icon(Icons.logout),
+                          suffixIcon: IconButton(
+                            onPressed: (){
+                              setState(() {
+                                notVisible=!notVisible;
+                              });
+                            },
+                            icon: Icon(notVisible==true?Icons.visibility:Icons.visibility_off),
+                          ),
+                          contentPadding: EdgeInsets.only(left: 20, right: 20),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                            borderSide: BorderSide(
+                              width: 1, color: Colors.deepOrange
+                            )
+                          )
+                        ),
+                      ),
+                  ),
+                ),
+                IconButton(onPressed: (){
+                  setState(() {
+                    this.query=queryTEC.text;
+                  });
+                },
+                    icon: Icon(Icons.search, color: Colors.deepOrange,))
+              ],
+            )
+          ],
+        )
       ),
     );
   }
