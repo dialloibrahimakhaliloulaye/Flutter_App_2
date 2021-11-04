5bc94590c0116ce2e0650d63cdaa56e1aa691d21
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-11-04 00:41:43 +0000

    first part

--- lib/pages/users/users.page.dart
+++ lib/pages/users/users.page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class UsersPage extends StatefulWidget {
 
@@ -11,11 +14,52 @@ class _UsersPageState extends State<UsersPage> {
   TextEditingController queryTEC=TextEditingController();
   String query='';
   bool notVisible=false;
+  dynamic data;
+  int totalPages=0;
+  int currentPage=0;
+  int pageSize=20;
+  ScrollController scrollController=ScrollController();
+  List<dynamic>items=[];
+
+  void _search(String query){
+    String url="https://api.github.com/search/users?q=${query}&per_page=$pageSize&page=$currentPage";
+    http.get(Uri.parse(url))
+      .then((response) {
+        setState(() {
+          data=json.decode(response.body);
+          items.addAll(data['items']);
+          if(data['total_count'] % pageSize ==0) {
+            totalPages = data['total_count'] ~/ pageSize;
+          } else totalPages= (data['total_count'] / pageSize).floor()+1;
+        });
+    })
+      .catchError((err){
+        print(err);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    scrollController.addListener(() {
+      if(scrollController.position.pixels==scrollController.position.maxScrollExtent){
+        setState(() {
+          if(currentPage<totalPages-1){
+            ++currentPage;
+            _search(query);
+          }
+
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Users => ${query}", style: Theme.of(context).textTheme.headline6,),),
+      appBar: AppBar(title: Text("Users => ${query} => $currentPage / $totalPages",
+        style: Theme.of(context).textTheme.headline6,),),
       body: Center(
         child: Column(
           children: [
@@ -55,11 +99,46 @@ class _UsersPageState extends State<UsersPage> {
                 ),
                 IconButton(onPressed: (){
                   setState(() {
+                    items=[];
+                    currentPage=0;
                     this.query=queryTEC.text;
+                    _search(query);
                   });
                 },
                     icon: Icon(Icons.search, color: Colors.deepOrange,))
               ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                controller: scrollController,
+                itemCount: items.length,
+                  itemBuilder: (context, index){
+                    return ListTile(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                backgroundImage: NetworkImage(items[index]['avatar_url']),
+                                radius: 38,
+                              ),
+                              SizedBox(width: 25,),
+                              Text("${items[index]['login']}", style: TextStyle(fontSize: 17),),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                child: Text("${items[index]['score']}", style: TextStyle(fontSize: 15),),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    );
+                  }
+              ),
             )
           ],
         )
